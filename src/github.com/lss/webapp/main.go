dad3c2420f20ed398da9adc5061de776ba8becab
Miles <milesasp@gmail.com>
2019-12-18 22:15:49 +0800

    use template composition

--- src/github.com/lss/webapp/main.go
+++ src/github.com/lss/webapp/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"log"
 	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 	templates := populdateTemplates()
 	http.HandleFunc("/", func(w http.ResponseWriter, r * http.Request) {
 		requestedFile := r.URL.Path[1:]
-		fmt.Println(r.URL.Path)
-		t := templates.Lookup(requestedFile + ".html")
+		t := templates[requestedFile + ".html"]
 		if t != nil {
 			err := t.Execute(w, nil)
 			if err != nil {
@@ -28,9 +28,35 @@ func main() {
 	// http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
 }
 
-func populdateTemplates() * template.Template {
-	result := template.New("templates")
+func populdateTemplates() map[string]*template.Template {
+	result := make(map[string]*template.Template)
 	const basePath = "templates"
-	template.Must(result.ParseGlob(basePath + "/*.html"))
+	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
+	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
+	dir, err := os.Open(basePath + "/content")
+	if err != nil {
+		panic("Failed to open template blocks directory: " + err.Error())
+	}
+	fis, err := dir.Readdir(-1)
+	if err != nil {
+		panic("Failed to read contents of content directory: " + err.Error())
+	}
+	for _, fi := range fis {
+		f, err := os.Open(basePath + "/content/" + fi.Name())
+		if err != nil {
+			panic("Failed to open template '" + fi.Name() + "'")
+		}
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic("Failed to read content from file '" + fi.Name() + "'")
+		}
+		f.Close()
+		tmpl := template.Must(layout.Clone())
+		_, err = tmpl.Parse(string(content))
+		if err != nil {
+			panic("Failed to parse contents of '" + fi.Name() + "' as template")
+		}
+		result[fi.Name()] = tmpl
+	}
 	return result
 }
