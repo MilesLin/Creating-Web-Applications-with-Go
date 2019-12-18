17347d018eb2b86f2b508183d9cdc364060f851a
Miles <milesasp@gmail.com>
2019-12-18 22:42:49 +0800

    use data-driven template

--- src/github.com/lss/webapp/main.go
+++ src/github.com/lss/webapp/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"creating-web-applications-go/src/github.com/lss/webapp/viewmodel"
 )
 
 func main() {
@@ -13,8 +14,16 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		requestedFile := r.URL.Path[1:]
 		template := templates[requestedFile+".html"]
+		var context interface{}
+		switch requestedFile {
+		case "shop":
+			context = viewmodel.NewShop()
+		default:
+			context = viewmodel.NewBase()
+		}
+
 		if template != nil {
-			err := template.Execute(w, nil)
+			err := template.Execute(w, context)
 			if err != nil {
 				log.Println(err)
 			}
