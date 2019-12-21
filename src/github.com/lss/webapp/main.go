2852eb3f2ae164ad3c9425f309a50771aa5011fe
Miles <milesasp@gmail.com>
2019-12-21 11:24:18 +0800

    failed to gzip

--- src/github.com/lss/webapp/main.go
+++ src/github.com/lss/webapp/main.go
@@ -7,12 +7,13 @@ import (
 	"os"
 
 	"creating-web-applications-go/src/github.com/lss/webapp/controller"
+	"creating-web-applications-go/src/github.com/lss/webapp/middleware"
 )
 
 func main() {
 	templates := populateTemplates()
 	controller.Startup(templates)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", new(middleware.GzipMiddleware))
 }
 
 func populateTemplates() map[string]*template.Template {
